d021313466e98fa0dc9cd4a6def06542a2d647ca
bhandd <bjorn1987@gmail.com>
2025-08-26 00:19:58 +0200

    skipped the angling app and updated the portfolio page

--- src/main/java/com/skibob/home/WebappApplication.java
+++ src/main/java/com/skibob/home/WebappApplication.java
@@ -2,12 +2,25 @@ package com.skibob.home;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class WebappApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(WebappApplication.class, args);
     }
 
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("name", "Björn Andersson");
+        model.addAttribute("title", "Högskoleingenjör i Datateknik – Portfolio och CV");
+        return "index";
+    }
 }
